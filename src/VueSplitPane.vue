169a2e3f823754b5726b750d232f925a92256761
feritarou <schwarz.f@web.de>
2023-10-09 00:50:41 +0200

    feat: add vertical mode

--- src/VueSplitPane.vue
+++ src/VueSplitPane.vue
@@ -1,17 +1,14 @@
 <template>
-  <div
-    class="split-pane"
-    :class="{ 'is-dragging': dragging }"
-    @mousemove="dragMove"
-    @mouseup="dragEnd"
-    @mouseleave="dragEnd"
-  >
-    <div class="split-pane-item" :style="{ width: splitLeft }">
-      <slot name="left" />
+  <div class="split-pane" :class="{ 'is-dragging': dragging }" @mousemove="dragMove" @mouseup="dragEnd"
+    @mouseleave="dragEnd">
+    <div class="split-pane-item" :style="{ [splitCssProp]: splitLeftOrTop }">
+      <slot name="left"></slot>
+      <slot name="top"></slot>
     </div>
     <div class="split-pane-gutter" @mousedown="dragStart"></div>
-    <div class="split-pane-item" :style="{ width: splitRight }">
-      <slot name="right" />
+    <div class="split-pane-item" :style="{ [splitCssProp]: splitRightOrBottom }">
+      <slot name="right"></slot>
+      <slot name="bottom"></slot>
     </div>
   </div>
 </template>
@@ -19,32 +16,50 @@
 <script>
 export default {
   name: 'VueSplitPane',
+  props: {
+    vertical: {
+      type: Boolean,
+      default: false,
+    },
+  },
   data() {
+    const splitCssProp = this.vertical ? 'height' : 'width';
     return {
       gutter: 11,
       split: 50,
       dragging: false,
+      splitCssProp,
     };
   },
   computed: {
-    splitLeft: function () {
+    splitLeftOrTop: function () {
       return `calc(${this.split}% - ${this.gutter}px)`;
     },
-    splitRight: function () {
+    splitRightOrBottom: function () {
       return `calc(${100 - this.split}% - ${this.gutter}px)`;
     },
   },
   methods: {
     dragStart(e) {
       this.dragging = true;
-      this.startX = e.pageX;
+      if (this.vertical) {
+        this.startY = e.pageY;
+      } else {
+        this.startX = e.pageX;
+      }
       this.startSplit = this.split;
     },
     dragMove(e) {
       if (this.dragging) {
-        const dx = e.pageX - this.startX;
-        const totalWidth = this.$el.offsetWidth;
-        this.split = this.startSplit + ~~((dx / totalWidth) * 100);
+        if (this.vertical) {
+          const dy = e.pageY - this.startY;
+          const totalHeight = this.$el.offsetHeight;
+          this.split = this.startSplit + ~~((dy / totalHeight) * 100);
+        } else {
+          const dx = e.pageX - this.startX;
+          const totalWidth = this.$el.offsetWidth;
+          this.split = this.startSplit + ~~((dx / totalWidth) * 100);
+        }
       }
     },
     dragEnd() {
